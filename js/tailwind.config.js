ac7ec11c98e6afc0a54052d1b0508bef368fb710
Md Rafiqul Islam <rafiqulislamemon05@gmail.com>
2023-11-06 17:08:12 +0600

    2nd commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                banner: '#1C1E53',
+                menuColor:'#BBBBCB',
+                hero: '#1C1E53',
+                buttonColor:'#FCD980',
+                work: '#F4F6FC',
+                para: '#282938b3',
+                heading: '#282938',
+                overlay:'#1c1e5380',
+            },
+            width:{
+                maincontainer: "1280px"
+            },
+            fontFamily:{
+                poppins: ["'Poppins', sans-serif"],
+            },
+            backgroundImage:{
+                form: "url('images/wiritng.png')",
+            },
+        }
+    }
+}
